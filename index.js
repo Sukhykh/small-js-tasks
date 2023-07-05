54b7dbfd4ddc0ba731f1e9ad2bbd408b914af504
Kostiantyn Sukhykh <konstantin.suhih@gmail.com>
2023-07-05 22:10:48 +0300

    up to 20 tasks

--- index.js
+++ index.js
@@ -102,3 +102,169 @@ const isCyclicShiftPossible2 = (str1, str2) => {
     }
     return false
 }
+
+/* Task 12 (сортування масиву) */
+const getTwoSortedArrays2 = arr => {
+    if (!Array.isArray(arr)) return 'Incorrect data'
+    const arrSpreader = main => main.reduce((closest, item, index) => {
+        const nextItem = main[index + 1];
+        if (nextItem) {
+            const diff = Math.abs(nextItem - item);
+            if (diff < closest.diff) {
+                closest.diff = diff;
+                closest.min = item;
+                closest.max = nextItem;
+            }
+        }
+        return closest
+    }, { min: null, max: null, diff: Infinity})
+    const sortedArr = [...arr].sort((a, b) => a - b)
+    const { minArr, maxArr } = sortedArr.reduce((result, item, index) => {
+        if (index % 2 === 0) {
+            const closest = arrSpreader(result.mainArr)
+            result.minArr.push(closest.min)
+            result.maxArr.push(closest.max)
+            result.mainArr.splice(result.mainArr.indexOf(closest.max), 1);
+            result.mainArr.splice(result.mainArr.indexOf(closest.min), 1);
+        }
+        return result
+    }, { minArr: [], maxArr: [], mainArr: [...sortedArr]})
+    return { minArr, maxArr }
+}
+
+/* Task 13 (форматування рядка) */
+const stringFormatting2 = str => {
+    if (typeof str !== 'string') return 'Incorrect data';
+    let formattedString = str.toLowerCase().split(' ').map(item => {
+        if (item.includes('http://') || item.includes('https://')) return "[посилання заборонено]";
+        if (item.includes('@')) return "[контакти заборонені]";
+        if (!isNaN(item) && item.length > 3) return "";
+        return item;
+    }).join(' ')
+    if (formattedString.length > str.length) setInterval(() => alert("Вам потрібна допомога?"), 5000)
+    return formattedString.slice(0, 1).toUpperCase() + formattedString.slice(1)
+}
+
+/* Task 14 (перевірка наявності всіх круглих дужок) */
+const checkingParentheses1 = str => {
+    if (typeof str !== 'string') return 'Incorrect data';
+    const stack = []
+    for (let i = 0 ; i < str.length ; i++) {
+        const symbol = str.charAt(i)
+        if (symbol === '(') stack.push(symbol)
+        if (symbol === ')') {
+            if (stack.length === 0) return 'Відповідність відкриваючих та закриваючих дужок не витримано.'
+            stack.pop()
+        }
+    }
+    if (stack.length !== 0) return 'Відповідність відкриваючих та закриваючих дужок не витримано.'
+    document.getElementById('task14').innerText = str;
+    document.getElementById('task14').style.userSelect = 'none';
+    document.addEventListener('copy', (event) => event.preventDefault());
+    document.addEventListener('contextmenu', (event) => event.preventDefault());
+    document.addEventListener('keydown', (event) => event.preventDefault());
+}
+
+/* Task 15 (генератор паролів) */
+const passwordGenerator1 = () => {
+    const gererator = number => Math.floor(Math.random() * number)
+    const availableValues = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    const length = Math.floor(Math.random() * 14 + 6)
+    const password = new Array(length).fill('*')
+    const underscoreIndex = gererator(length);
+    let firstUppercaseLetterIndex = gererator(length);
+    while ( firstUppercaseLetterIndex === underscoreIndex ) {
+        firstUppercaseLetterIndex = gererator(length);
+    }
+    let secondUppercaseLetterIndex = gererator(length);
+    while ( secondUppercaseLetterIndex === firstUppercaseLetterIndex || secondUppercaseLetterIndex === underscoreIndex) {
+        secondUppercaseLetterIndex = gererator(length);
+    }
+    password[underscoreIndex] = '_';
+    let counter = 0;
+    for (let i = 0 ; i < length ; i++) {
+        if (password[i] === '_') continue
+        if (i === firstUppercaseLetterIndex || i === secondUppercaseLetterIndex) {
+            password[i] = availableValues[gererator(26)]
+            continue
+        }
+        if (counter === 5) {
+            password[i] = availableValues[gererator(52)]
+            continue
+        }
+        password[i] = availableValues[gererator(availableValues.length)]
+        if (!isNaN(password[i])) {
+            counter++
+            if (!isNaN(password[i+1]) || !isNaN(password[i-1])) password[i] = availableValues[gererator(52)]
+        }
+    }
+    return password.join('')
+}
+
+/* Task 16 (перестановка елементів масиву) */
+const arrayShuffle3 = arr => {
+    if (!Array.isArray(arr)) return 'Incorrect data'
+    const sortedArr = arr.sort((a, b) => a - b);
+    const result = new Array(sortedArr.length)
+    let counter = 0;
+    for (let i = 0 ; i < sortedArr.length ; i++) {
+        if ( i % 2 === 0 ) {
+            result[i/2] = sortedArr[i]
+        } else {
+            result[result.length - i + counter] = sortedArr[i]
+            counter ++
+        }
+    }
+    return result
+}
+
+/* Task 17 (відсортувати рядок за частотою появи символів) */
+const sortSymbolsInString2 = str => {
+    if (typeof str !== 'string') return 'Incorrect data'
+    const stringToObject = str.toLowerCase().split('').reduce((acum, item) => {
+        acum[item] = (acum[item] || 0) + 1;
+        return acum;
+    }, {})
+    return Object
+    .entries(stringToObject)
+    .map(([key, value]) => new Array(value).fill(key))
+    .sort((a, b) => b.length - a.length)
+    .flat()
+    .join('');
+}
+
+/* Task 18 (пошук найдовшого загального рядка) */
+const longestCommonString2 = (str1, str2) => {
+    if (typeof str1 !== 'string' || typeof str2 !== 'string') return 'Incorrect data'
+    const column = str1.length + 1;
+    const row = str2.length + 1;
+    const matrix = new Array(row).fill(0).map(() => new Array(column).fill(0));
+    let maxLength = 0;
+    let lastIndex = 0;
+    for (let i = 1 ; i < row ; i++) {
+        if (str2[i-1] === " ") continue
+        for (let j = 1 ; j < column ; j++) {
+            if (str1[j-1] === " ") continue
+            if (str2[i-1] === str1[j-1]) matrix[i][j] = matrix[i-1][j-1] + 1;
+            if (matrix[i][j] > maxLength) {
+                maxLength = matrix[i][j]
+                lastIndex = i
+            }
+        }
+    }
+    return str2.slice(lastIndex - maxLength , maxLength)
+}
+
+/* Task 19 (шифр Цезаря) */
+const caesarsCipher3 = (str, number) => {
+    if (typeof str !== 'string' || isNaN(number)) return 'Incorrect data';
+    const generalString = 'abcdefghijklmnopqrstuvwxyz';
+    return str
+        .split('')
+        .reduce((acum, item, index) => {
+            if (!generalString.includes(item.toLowerCase())) return acum += item
+                const indexValue = generalString.indexOf(item.toLowerCase())
+                const char = generalString[(indexValue + number) % generalString.length]
+                return str[index] === str[index].toUpperCase() ? acum += char.toUpperCase() : acum += char
+        }, '')
+}
